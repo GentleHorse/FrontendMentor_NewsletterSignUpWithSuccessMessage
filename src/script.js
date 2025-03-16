b2554e2a9d89d992a43639ab4737733dc5e9aca0
GentleHorse <anch.abc.t@gmail.com>
2025-03-16 20:35:44 +0100

    Added the email input focus styling logic

--- src/script.js
+++ src/script.js
@@ -2,15 +2,23 @@ const signupCard = document.getElementById("card-signup");
 const successCard = document.getElementById("card-success");
 const signupButton = document.getElementById("signup-button");
 const dismissButton = document.getElementById("dismiss-button");
+const emailUserInput = document.getElementById("email");
 
 // Default states
 signupCard.style.display = "block";
 successCard.style.display = "none";
 
+emailUserInput.addEventListener("focus", () => {
+  signupButton.style.background =
+    "linear-gradient(90deg, rgba(255,106,58,1) 0%, rgba(255,82,123,1) 100%)";
+});
+
+emailUserInput.addEventListener("focusout", () => {
+  signupButton.style.background = "#242742";
+});
+
 signupButton.addEventListener("click", (event) => {
   event.preventDefault();
-
-  
 
   signupCard.style.display = "none";
   successCard.style.display = "block";
