33043de2f1e3dafa701df566b1c54eda99b68f7a
GentleHorse <anch.abc.t@gmail.com>
2025-03-16 20:14:18 +0100

    Implemented the logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const signupCard = document.getElementById("card-signup");
+const successCard = document.getElementById("card-success");
+const signupButton = document.getElementById("signup-button");
+const dismissButton = document.getElementById("dismiss-button");
+
+// Default states
+signupCard.style.display = "block";
+successCard.style.display = "none";
+
+signupButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  signupCard.style.display = "none";
+  successCard.style.display = "block";
+});
+
+dismissButton.addEventListener("click", () => {
+  signupCard.style.display = "block";
+  successCard.style.display = "none";
+});
